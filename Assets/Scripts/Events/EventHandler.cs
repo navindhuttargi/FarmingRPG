7f1b456cac775056354fe7cfef0391e196f41ade
navinsd <navindhuttargi@gmail.com>
2022-06-14 17:39:31 +0530

    added event and EventHandler script

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,28 @@
+﻿public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool carring, ToolEffect toolEffect,
+                                     bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+                                     bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+                                     bool isPickingToolRight, bool isPickingToolLeft, bool isPickingToolUp, bool isPickingToolDown,
+                                     bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+                                     bool isIdleRight, bool isIdleLEft, bool isIdleUp, bool isIdleDown);
+
+public static class EventHandler
+{
+    public static event MovementDelegate MovementEvent;
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool carring, ToolEffect toolEffect,
+                                     bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+                                     bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+                                     bool isPickingToolRight, bool isPickingToolLeft, bool isPickingToolUp, bool isPickingToolDown,
+                                     bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+                                     bool isIdleRight, bool isIdleLEft, bool isIdleUp, bool isIdleDown)
+    {
+        if (MovementEvent != null)
+        {
+            MovementEvent.Invoke(inputX, inputY, isWalking, isRunning, isIdle, carring, toolEffect,
+                                     isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                                     isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                                     isPickingToolRight, isPickingToolLeft, isPickingToolUp, isPickingToolDown,
+                                     isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                                     isIdleRight, isIdleLEft, isIdleUp, isIdleDown);
+        }
+    }
+}
