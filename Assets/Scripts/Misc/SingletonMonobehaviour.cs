5ea8b6247fa006ae242fadc2248f039c09764975
navinsd <navindhuttargi@gmail.com>
2022-06-14 15:31:25 +0530

    added player class and Abstract singleton class

---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instace;
+    public static T Instance
+    {
+        get => instace;
+    }
+    protected virtual void Awake()
+    {
+        if (instace == null)
+            instace = this as T;
+        else
+            Destroy(gameObject);
+    }
+}
